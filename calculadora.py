3f568f09b0c28dff167eb4f8e71d4f2fee66ef2c
K0JAMMM <kaueorlandini01@gmail.com>
2026-07-30 20:32:18 -0300

    alunos

--- calculadora.py
+++ calculadora.py
@@ -15,6 +15,6 @@ def calcular_total(itens, desconto_percentual=0):
 
     # Defeito proposital:
     # o desconto percentual esta sendo subtraido como valor monetario.
-    total = subtotal - desconto_percentual
+    total = subtotal * ( 1 - desconto_percentual)
 
     return round(total, 2)
